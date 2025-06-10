44b010b4976173c6844f2537301196fdc75ee57f
Arslan9191 <skillik2408@gmail.com>
2025-06-10 23:12:08 +0300

    Добавлена папка TestProject4

---
+++ TestProject4/Test1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoinUp;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TestProject4
+{
+    [TestClass]
+    public class MainWindowTests
+    {
+        [TestMethod]
+        public void PressOrganizerButt_ShouldSetEnterFlagToO_WhenOrganizerButtonClicked()
+        {
+            // Arrange: создаем экземпляр MainWindow
+            var mainWindow = new MainWindow();
+
+            // Act: симулируем нажатие на кнопку "Я - организатор"
+            var organizerButton = new Button();
+            mainWindow.PressOrganizerButt(organizerButton, null);
+
+            // Assert: проверяем, что флаг enterFlag равен 'o'
+            Assert.AreEqual('o', MainWindow.enterFlag, "Флаг enterFlag должен быть 'o' для организатора.");
+        }
+
+        [TestMethod]
+        public void PressMemberButt_ShouldSetEnterFlagToM_WhenMemberButtonClicked()
+        {
+            // Arrange: создаем экземпляр MainWindow
+            var mainWindow = new MainWindow();
+
+            // Act: симулируем нажатие на кнопку "Я - участник"
+            var memberButton = new Button();
+            mainWindow.PressMemberButt(memberButton, null);
+
+            // Assert: проверяем, что флаг enterFlag равен 'm'
+            Assert.AreEqual('m', MainWindow.enterFlag, "Флаг enterFlag должен быть 'm' для участника.");
+        }
+    }
+}
